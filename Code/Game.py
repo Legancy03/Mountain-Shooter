ecb588a69b1be47c2314465f87231ef9cf623dba
Lucas <lucasfreitasf@proton.me>
2024-10-18 17:57:19 -0300

    Changing menu

--- Code/Game.py
+++ Code/Game.py
@@ -13,10 +13,7 @@ class Game:
         self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
-
-
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-
