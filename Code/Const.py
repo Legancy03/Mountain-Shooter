c8388e879dc536490f9e07647f1ecdb3d594cb53
Lucas <lucasfreitasf@proton.me>
2024-10-20 00:15:46 -0300

    Hit Box Collisions HUD Score

--- Code/Const.py
+++ Code/Const.py
@@ -1,9 +1,11 @@
 # C
 import pygame
 
-COLOR_ORANGE = (255, 128, 0)
-COLOR_WHITE = (255, 255, 255)
-COLOR_YELLOW = (225, 225, 0)
+C_ORANGE = (255, 128, 0)
+C_YELLOW = (225, 225, 0)
+C_WHITE = (255, 255, 255)
+C_GREEN = (0, 128, 0)
+C_CYAN = (0, 128, 128)
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
@@ -14,7 +16,12 @@ ENTITY_SPEED = {
     'Level1Bg3': 3,
     'Level1Bg4': 4,
     'Level1Bg5': 5,
-    'Level1Bg6': 6,
+    'Level1Bg6': 5,
+    # 'Level2Bg0': 6,
+    # 'Level2Bg1': 6,
+    # 'Level2Bg2': 6,
+    # 'Level2Bg3': 6,
+    # 'Level2Bg4': 6,
     'Player1': 3,
     'Player1Shot': 4,
     'Player2': 3,
@@ -24,6 +31,52 @@ ENTITY_SPEED = {
     'Enemy2': 1,
     'Enemy2Shot': 2,
 
+}
+
+ENTITY_DAMAGE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    # 'Level2Bg0': 0,
+    # 'Level2Bg1': 0,
+    # 'Level3Bg2': 0,
+    # 'Level2Bg3': 0,
+    # 'Level2Bg4': 0,
+    'Player1': 1,
+    'Player1Shot': 25,
+    'Player2': 1,
+    'Player2Shot': 28,
+    'Enemy1': 1,
+    'Enemy1Shot': 20,
+    'Enemy2': 1,
+    'Enemy2Shot': 15,
+}
+
+ENTITY_SCORE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    # 'Level2Bg0': 0,
+    # 'Level2Bg1': 0,
+    # 'Level3Bg2': 0,
+    # 'Level2Bg3': 0,
+    # 'Level2Bg4': 0,
+    'Player1': 0,
+    'Player1Shot': 0,
+    'Player2': 0,
+    'Player2Shot': 0,
+    'Enemy1': 100,
+    'Enemy1Shot': 0,
+    'Enemy2': 125,
+    'Enemy2Shot': 0,
 }
 
 ENTITY_SHOT_DELAY = {
@@ -78,8 +131,6 @@ PLAYER_KEY_SHOT = {'Player1': pygame.K_RCTRL,
 
 # S
 SPAWN_TIME = 4000
-
-
 
 # W
 WIN_WIDTH = 576
