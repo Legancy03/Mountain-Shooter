421d8d0dab74e1812bec265fda9b3f05aceaabb6
Lucas <lucasfreitasf@proton.me>
2024-10-20 01:02:47 -0300

    Level 2 implementation

--- Code/Const.py
+++ Code/Const.py
@@ -9,6 +9,7 @@ C_CYAN = (0, 128, 128)
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
+EVENT_TIMEOUT = pygame.USEREVENT + 2
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
@@ -17,11 +18,11 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 5,
-    # 'Level2Bg0': 6,
-    # 'Level2Bg1': 6,
-    # 'Level2Bg2': 6,
-    # 'Level2Bg3': 6,
-    # 'Level2Bg4': 6,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
     'Player1': 3,
     'Player1Shot': 4,
     'Player2': 3,
@@ -41,11 +42,11 @@ ENTITY_DAMAGE = {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
-    # 'Level2Bg0': 0,
-    # 'Level2Bg1': 0,
-    # 'Level3Bg2': 0,
-    # 'Level2Bg3': 0,
-    # 'Level2Bg4': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
     'Player1': 1,
     'Player1Shot': 25,
     'Player2': 1,
@@ -64,11 +65,11 @@ ENTITY_SCORE = {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
-    # 'Level2Bg0': 0,
-    # 'Level2Bg1': 0,
-    # 'Level3Bg2': 0,
-    # 'Level2Bg3': 0,
-    # 'Level2Bg4': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
     'Player1': 0,
     'Player1Shot': 0,
     'Player2': 0,
@@ -94,11 +95,11 @@ ENTITY_HEALTH = {
     'Level1Bg4': 999,
     'Level1Bg5': 999,
     'Level1Bg6': 999,
-    # 'Level2Bg0': 999,
-    # 'Level2Bg1': 999,
-    # 'Level2Bg2': 999,
-    # 'Level2Bg3': 999,
-    # 'Level2Bg4': 999,
+    'Level2Bg0': 999,
+    'Level2Bg1': 999,
+    'Level2Bg2': 999,
+    'Level2Bg3': 999,
+    'Level2Bg4': 999,
     'Player1': 300,
     'Player1Shot': 1,
     'Player2': 300,
@@ -131,6 +132,10 @@ PLAYER_KEY_SHOT = {'Player1': pygame.K_RCTRL,
 
 # S
 SPAWN_TIME = 4000
+
+# T
+TIMEOUT_STEP = 100  # 10 milliseconds
+TIMEOUT_LEVEL = 20000  # 20 segundos
 
 # W
 WIN_WIDTH = 576
